16d9271ef865e2036861f16b6577423c3e0ac8a6
Herman Leung <leung.hm@gmail.com>
2020-07-08 17:38:07 +0800

    New exercise in progress

--- WORK_IN_PROGRESS_shortest_palindrome.py
+++ WORK_IN_PROGRESS_shortest_palindrome.py
@@ -1,6 +1,10 @@
 '''
-WIP
+WORK IN PROGRESS
 [from dailycodingproblem.com #34]
+# Log 2020-07-08 
+# -- two tests fail (words with a partial palindrome in the middle)
+# -- execution time extremely slow when string > 6 chars, because of 
+#    exponential increase in number of permutations, need a better method
 
 Given a string, find the palindrome that can be made by inserting the fewest 
 number of characters as possible anywhere in the word. If there is more than 
@@ -18,78 +22,109 @@ from collections import defaultdict
 from itertools import permutations
 
 
+def is_palindrome(string):
+    mid_point = round(len(string) / 2)
+    if ''.join(reversed(string[:mid_point])) == string[-mid_point:]:
+        return True
+    return False
+
+def has_original_sequence(candidate, string):
+    """Filter out palindromes where the original string is no longer in its
+    original sequence"""
+    copy = list(string)
+    for i in range(len(candidate) - 1, -1, -1):
+        if len(copy) == 0:
+            break
+        if copy[-1] == candidate[i]:
+            copy.pop()
+
+    if len(copy) == 0:
+        return True
+    return False
+
+def has_partial_palindrome(string, is_reverse=False):
+    copy = ''.join(reversed(string)) if is_reverse else string
+    palindromes = []
+
+    mid_point = round(len(copy) / 2)
+    before = list(reversed(copy[:mid_point]))
+    after = list(copy[mid_point:])
+
+    palindrome_start = mid_point - 1
+    for _ in range(mid_point):
+        if before == after[:len(before)]:
+            break
+        else:
+            after = [before[0]] + after
+            before = before[1:]
+            palindrome_start -= 1
+
+    if palindrome_start >= 0:
+        candidate = ''.join(
+            reversed(copy[palindrome_start * 2 + 2:])) + copy
+        if is_reverse:
+            candidate = ''.join(reversed(candidate))
+        palindromes.append(candidate)
+
+    return palindromes
+
 def palindrome(string):
-    palindrome_candidates = []
-    
-    # google type palindrome
+    # check if already a palindrome
+    if is_palindrome(string):
+        return string
+
+    had_partial_palindromes = []
     letters = defaultdict(int)
-    
     for s in string:
         letters[s] += 1
 
+    had_partial_palindromes += has_partial_palindrome(string)
+    had_partial_palindromes += has_partial_palindrome(string, is_reverse=True)
+
     letters_to_add = []
-    more_letters_to_add = []
+    if len(set(string)) == len(string):
+        for i, s in enumerate(string):
+            letters_to_add.append(string + string[:i] + string[i+1:])
 
-    if len(letters) < len(string):
-        for key in letters:
-            if letters[key] == 1:
-                letters_to_add.append(key)
-            if letters[key] > 2:
-                more_letters_to_add.append(key)
-
-        jumbles = [s for s in string] + letters_to_add
-        jumbles += more_letters_to_add
-        palindrome_candidates = permutations(jumbles)
-
-        for key in letters:
-            if letters[key] > 2:
-                letters_to_add.append(key)
-
-    # race type palindrome
     else:
-        for s in string:
-            jumbles = [c for c in string] + [c for c in string if c != s]
-            palindrome_candidates += [p for p in permutations(jumbles)]
+        for i in range(1, max(letters.values()) + 1):
+            to_add = string
+            for key in letters:
+                if letters[key] == i:
+                    to_add += key * i
+            letters_to_add.append(to_add)
 
-    palindromes = []
-    for candidate in palindrome_candidates:
-        c = ''.join(candidate)
-        mid_point = round(len(c) / 2)
-        first_half = ''.join(reversed(c[:mid_point]))
-        back_half = c[mid_point:]
+    palindromes = list(had_partial_palindromes)
+    for l in letters_to_add:
+        p = permutations(l)
+        for item in p:
+            if (
+                is_palindrome(''.join(item)) and 
+                has_original_sequence(''.join(item), string)
+            ):
+                palindromes.append(''.join(item))
 
-        if len(c) % 2 != 0:
-            back_half = c[-mid_point:]
-
-        if first_half == back_half:
-            copy = list(string)
-
-            for i in range(len(c) - 1, -1, -1):
-                if len(copy) == 0:
-                    break
-
-                if copy[-1] == c[i]:
-                    copy.pop()
-
-            if len(copy) == 0:
-                palindromes.append(''.join(c))
-
-    palindromes.sort(key=lambda x: (x, len(x)))
+    palindromes.sort(key=lambda x: (len(x), x))
     return palindromes[0]
+
 
 '''
 # TESTS
 
+# Words with no repeated letters
 palindrome('race') == 'ecarace'
-palindrome('google') == 'elgoogle'
+palindrome('pear') == 'pearaep'
+palindrome('stare') == 'erastsare'  
+
+# Words with partial palindromes 
+palindrome('google') == 'elgoogle'  # front
+palindrome('alool') == 'aloola'  # end
+palindrome('timid') == 'dtimitd'  # inside  # FAIL
+palindrome('vvdfaa') == 'aafdvvdfaa'  # both front and end 
+palindrome('ttimidd') == 'ddttimittdd'  # everywhere  # FAIL
+
+# Words with some repeated letters but no partial palindromes
 palindrome('ravage') == 'egravvarge'
 palindrome('effort') == 'etrofforte'
 palindrome('leeves') == 'lsevevesl'
-palindrome('aardvark') == 'kravdraardvark'
-palindrome('crazyeights')
-
-
-racecar
-
-
 '''
