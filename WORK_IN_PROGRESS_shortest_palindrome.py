f20e9746b334e7b5ff815b6d1f12cff6641bf579
Herman Leung <leung.hm@gmail.com>
2020-07-09 11:56:05 +0800

    Update exercise

--- WORK_IN_PROGRESS_shortest_palindrome.py
+++ WORK_IN_PROGRESS_shortest_palindrome.py
@@ -1,10 +1,9 @@
 '''
 WORK IN PROGRESS
 [from dailycodingproblem.com #34]
-# Log 2020-07-08 
-# -- two tests fail (words with a partial palindrome in the middle)
-# -- execution time extremely slow when string > 6 chars, because of 
-#    exponential increase in number of permutations, need a better method
+# Log 2020-07-09 
+# -- one test fails
+# -- execution time sped up now by checking permutations in smaller batches
 
 Given a string, find the palindrome that can be made by inserting the fewest 
 number of characters as possible anywhere in the word. If there is more than 
@@ -18,8 +17,9 @@ by adding three letters, but "ecarace" comes first alphabetically.
 
 As another example, given the string "google", you should return "elgoogle".
 '''
+
 from collections import defaultdict
-from itertools import permutations
+from itertools import permutations, combinations
 
 
 def is_palindrome(string):
@@ -42,67 +42,43 @@ def has_original_sequence(candidate, string):
         return True
     return False
 
-def has_partial_palindrome(string, is_reverse=False):
-    copy = ''.join(reversed(string)) if is_reverse else string
+def mirror(string):
+    single_pivot_mirror = ''.join(reversed(string)) + string[1:]
+    full_mirror = ''.join(reversed(string)) + string
+    return [single_pivot_mirror, full_mirror]
+
+def permutate_mirror_and_check(candidate, string):
     palindromes = []
-
-    mid_point = round(len(copy) / 2)
-    before = list(reversed(copy[:mid_point]))
-    after = list(copy[mid_point:])
-
-    palindrome_start = mid_point - 1
-    for _ in range(mid_point):
-        if before == after[:len(before)]:
-            break
-        else:
-            after = [before[0]] + after
-            before = before[1:]
-            palindrome_start -= 1
-
-    if palindrome_start >= 0:
-        candidate = ''.join(
-            reversed(copy[palindrome_start * 2 + 2:])) + copy
-        if is_reverse:
-            candidate = ''.join(reversed(candidate))
-        palindromes.append(candidate)
-
+    for p in permutations(candidate):
+        maybes = mirror(''.join(p))
+        maybes = [m for m in maybes 
+                  if (is_palindrome(m) and has_original_sequence(m, string))]
+        palindromes += maybes
+    palindromes.sort(key=lambda x: (len(x), x))
     return palindromes
 
 def palindrome(string):
-    # check if already a palindrome
-    if is_palindrome(string):
-        return string
-
-    had_partial_palindromes = []
     letters = defaultdict(int)
     for s in string:
         letters[s] += 1
-
-    had_partial_palindromes += has_partial_palindrome(string)
-    had_partial_palindromes += has_partial_palindrome(string, is_reverse=True)
-
-    letters_to_add = []
-    if len(set(string)) == len(string):
-        for i, s in enumerate(string):
-            letters_to_add.append(string + string[:i] + string[i+1:])
-
-    else:
-        for i in range(1, max(letters.values()) + 1):
-            to_add = string
-            for key in letters:
-                if letters[key] == i:
-                    to_add += key * i
-            letters_to_add.append(to_add)
-
-    palindromes = list(had_partial_palindromes)
-    for l in letters_to_add:
-        p = permutations(l)
-        for item in p:
-            if (
-                is_palindrome(''.join(item)) and 
-                has_original_sequence(''.join(item), string)
-            ):
-                palindromes.append(''.join(item))
+    
+    basic = ''.join(letters.keys())
+    palindromes = permutate_mirror_and_check(basic, string)
+    
+    expanded_1 = basic
+    for i in range(2, max(letters.values()) + 1):
+        expanded_2 = basic
+        for key in letters:  # problem here, adding keys in specific order 
+                             # not accounting for permutations in order, but
+                             # if add permutations, processing time soars
+            expanded_3 = basic
+            if letters[key] == i:
+                expanded_1 += key
+                expanded_2 += key
+                expanded_3 += key
+                palindromes += permutate_mirror_and_check(expanded_3, string)
+            palindromes += permutate_mirror_and_check(expanded_2, string)
+        palindromes += permutate_mirror_and_check(expanded_1, string)
 
     palindromes.sort(key=lambda x: (len(x), x))
     return palindromes[0]
@@ -116,15 +92,18 @@ palindrome('race') == 'ecarace'
 palindrome('pear') == 'pearaep'
 palindrome('stare') == 'erastsare'  
 
-# Words with partial palindromes 
-palindrome('google') == 'elgoogle'  # front
-palindrome('alool') == 'aloola'  # end
-palindrome('timid') == 'dtimitd'  # inside  # FAIL
-palindrome('vvdfaa') == 'aafdvvdfaa'  # both front and end 
-palindrome('ttimidd') == 'ddttimittdd'  # everywhere  # FAIL
+# Words with partial palindromes on the edges
+palindrome('google') == 'elgoogle'  
+palindrome('alool') == 'aloola'  
+palindrome('vvdfaa') == 'aafdvvdfaa'  
+palindrome('aardvark') == 'akrardvdrarka'
 
-# Words with some repeated letters but no partial palindromes
-palindrome('ravage') == 'egravvarge'
+# Words with partial palindromes inside
+palindrome('timid') == 'dtimitd'  
+palindrome('ravage') == 'egravarge'
 palindrome('effort') == 'etrofforte'
 palindrome('leeves') == 'lsevevesl'
+
+# Words with partial palindromes everywhere
+palindrome('ttimidd') == 'ddttimittdd'  # FAIL: returns 'ttimiddimitt'
 '''
